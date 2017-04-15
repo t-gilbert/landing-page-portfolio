db8182ef3393ebdc193bd808239c09fcd5e198b9
T.gilbert <tgilbertstudio@gmail.com>
2017-04-15 19:09:27 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+landing-page portfolio
+
+Js files used:
+
+1) jQuery
+
+2) Bootstrap
+
+3) Hexagon-Buttons
+
+4) scrolla
+
+5) ekko-lightbox 
+
+6) jquery-appear
+
+7) jquery-easing
+
+8) jqBootstrapValidation
---
+++ vendor/progressbar/progressbar.js
@@ -0,0 +1,13 @@
+//Slider
+	$(document).ready(function(){
+
+		jQuery('.skillbar').each(function() {
+			jQuery(this).appear(function() {
+				jQuery(this).find('.count-bar').animate({
+					width:jQuery(this).attr('data-percent')
+				},3000);
+				var percent = jQuery(this).attr('data-percent');
+				jQuery(this).find('.count').html('<span>' + percent + '</span>');
+			});
+		});
+	});
